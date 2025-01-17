5f47805cc6746ec946df84205a4a369b187b3b91
Richard <richard.esclima@gmail.com>
2025-01-17 19:31:54 -0300

    feat: implementa leitura de teclado matricial

--- atividade-1.c
+++ atividade-1.c
@@ -11,12 +11,55 @@ const char KEYPAD[4][4] = {
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}};
 
+char scan_keypad()
+{
+    for (int row = 0; row < 4; row++)
+    {
+        gpio_put(ROW_PINS[row], 0); // Ativa a linha colocando em LOW
+        for (int col = 0; col < 4; col++)
+        {
+            if (!gpio_get(COL_PINS[col]))
+            {
+                gpio_put(ROW_PINS[row], 1); // Restaura a linha para HIGH
+                return KEYPAD[row][col];    // Retorna a tecla pressionada
+            }
+        }
+        gpio_put(ROW_PINS[row], 1); // linha volta para HIGH após a varredura
+    }
+    return '\0'; // Nenhuma tecla pressionada
+}
+
 int main()
 {
 
+    stdio_init_all();
+
+    // Configuração das linhas do teclado como saída
+    for (int i = 0; i < 4; i++)
+    {
+        gpio_init(ROW_PINS[i]);
+        gpio_set_dir(ROW_PINS[i], GPIO_OUT);
+        gpio_put(ROW_PINS[i], 1); // nível alto por padrão
+    }
+
+    // Configuração das colunas do teclado
+    for (int i = 0; i < 4; i++)
+    {
+        gpio_init(COL_PINS[i]);
+        gpio_set_dir(COL_PINS[i], GPIO_IN);
+        gpio_pull_up(COL_PINS[i]); // pull-up interno
+    }
     while (1)
     {
-        printf("ola mundo!");
+        char key = scan_keypad();
+        if (key != '\0')
+        { // Se alguma tecla foi pressionada
+            printf("tecla pressionada: %c\n", key);
+            if (key == 'A')
+            {
+                // e aqui vem a lógica da tecla
+            }
+        }
         sleep_ms(100);
     }
 
