381fc267d3b4a881f9f0f9761f215da9276a0b37
Richard <richard.esclima@gmail.com>
2025-01-17 19:29:31 -0300

    feat: realiza o mapeamento das teclas do teclado 4X4 para a leitura posterior

--- atividade-1.c
+++ atividade-1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+const uint ROW_PINS[4] = {2, 3, 8, 9};     // Linhas: R1, R2, R3, R4
+const uint COL_PINS[4] = {20, 19, 18, 17}; // Colunas: C1, C2, C3, C4
+
+// Mapeamento das teclas
+const char KEYPAD[4][4] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
+
 int main()
 {
 
